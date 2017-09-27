b51f923fd19b8021182554fce439990da7d85e4a
jcristol <joshcristol@gmail.com>
2017-09-26 22:33:05 -0500

    set index

--- flask-app/main.py
+++ flask-app/main.py
@@ -2,19 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/form')
+@app.route('/')
 def form():
-    return render_template('form.html')
-
-@app.route('/submitted', methods=['POST'])
-def submitted_form():
-    name = request.form['name']
-    email = request.form['email']
-    site = request.form['site_url']
-    comments = request.form['comments']
-    return render_template(
-    'submitted_form.html',
-    name=name,
-    email=email,
-    site=site,
-    comments=comments)
+    return render_template('index.html')

6136e835a34f4c945bc9002b8344e0deaffa2413
epersico <elliot.persico@gmail.com>
2017-10-22 20:04:04 -0500

    added TeamStats model and columns to models.py

--- app/models.py
+++ app/models.py
@@ -10,8 +10,8 @@ class Player(db.model):
     first_name = db.Column(db.String(50))
     birth_date = db.Column(db.String(50))
     high_school = db.Column(db.String(50))
-    weight = db.Column(db.String(50))
-    height = db.Column(db.String(50))
+    weight = db.Column(db.String(50)) #in lbs
+    height = db.Column(db.String(50)) #inches
     position = db.Column(db.String(50))
     jersey = db.Column(db.String(50))
     rookie_year = db.Column(db.String(50))
@@ -35,8 +35,8 @@ class Team(db.model)
     
     __tablename__ = 'teams'
 
-    id = db.Column(db.String(50), primary_key=True)
-    team_alias = db.Column(db.String(50))
+    id = db.Column(db.String(50))
+    team_alias = db.Column(db.String(50),primary_key=True)
     team_name = db.Column(db.String(50))
     team_market = db.Column(db.String(50))
     conference = db.Column(db.String(50))
@@ -44,3 +44,14 @@ class Team(db.model)
     venue_name = db.Column(db.String(50))
     venue_location = db.Column(db.String(50))
     pick_link = db.Column(db.String(50))
+
+class TeamStats(db.model)
+    id = db.Column(db.String(50),primary_key=True)
+    team_alias = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+    overall_rank = db.Column(db.Integer)
+    conference_rank = db.Column(db.Integer)
+    division_rank = db.Column(db.Integer)
+    season_wins = db.Column(db.Integer)
+    season losses = db.Column(db.Integer)
+    

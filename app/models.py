c06d614d81f04c4dc38659ef5bac81f46c6e7815
epersico <elliot.persico@gmail.com>
2017-10-22 19:48:29 -0500

    added columns to player/ coach model

--- app/models.py
+++ app/models.py
@@ -2,4 +2,11 @@
 from app import db
 
 class Player(db.model):
-    Name = db.Column(db.Integer)
+    Name = db.Column(db.String(50))
+
+class Coach(db.model):
+    id = db.Column(db.String(50) ,primary_key=True)
+    lastName = db.Column(db.String(50))
+    firstName = db.Column(db.String(50))
+    position = db.Column(db.String(50))
+    team = db.Column()

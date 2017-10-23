042f1a41c8369d6525d612b9a3b719ea3dab52f9
epersico <elliot.persico@gmail.com>
2017-10-22 19:33:41 -0500

    added basic playerlist and player calls

--- app/app.py
+++ app/app.py
@@ -1,7 +1,12 @@
 #!flask/bin/python
 
 from flask import Flask, jsonify
-from flask_restless import APIManager
 from flask_sqlalchemy import SQLAlchemy
 
+@app.route('/players', methods = ['GET'])
+def playerIndex():
+    return jsonify({'players': Player.query.all()})
 
+@app.route('/players/<int::id>')
+def get_player(id):
+    return jsonify({'Player': Player.query.get(id)})

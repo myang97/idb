ed4280b0f4ac77a8d2fa880309d3e069e1b82872
epersico <elliot.persico@gmail.com>
2017-10-22 22:06:00 -0500

    added other possible routes to app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-from models import Player
+from models import Player, Coach
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dummy:dummy@35.192.125.156/nfldata'
@@ -21,6 +21,24 @@ def playerIndex():
 @app.route('/players/<string:id>',methods = ['GET'])
 def get_player(id):
     return json.dumps(Player.query.get(id).as_dict())
+
+@app.route('/coaches', methods = ['GET'])
+def coachIndex():
+    return json.dumps([u.as_dict() for u in Coach.query.all()])
+
+@app.route('/coaches/<string:id>',methods = ['GET'])
+def get_coach(id):
+    return json.dumps(Team.query.get(id).as_dict())
+
+@app.route('/teams', methods = ['GET'])
+def teamIndex():
+=    return json.dumps([u.as_dict() for u in Coach.query.all()])
+
+@app.route('/teams/<string:team_alias>',methods = ['GET'])
+def get_team(id):
+    return json.dumps(Team.query.get(team_alias).as_dict())
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
